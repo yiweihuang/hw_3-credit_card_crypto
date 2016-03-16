024d26b0922856f8deb3f36a8253a1f678c5cb3b
yiweihuang <n1993212@gmail.com>
2016-03-16 18:01:47 +0800

    Add Caesar Cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,6 +7,8 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      letters = [*'a'..'z']
+      document.chars.select {|c| letters.include?(c.downcase)}.map {|c| (c.ord + key) >= 'z'.ord ? (c.ord + key - 26).chr : (c.ord + key).chr}.join
     end
 
     # Decrypts String document using integer key
@@ -16,6 +18,8 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      letters = [*'a'..'z']
+      document.chars.select {|c| letters.include?(c.downcase)}.map {|c| (c.ord - key) < 'a'.ord ? (c.ord - key + 26).chr : (c.ord - key).chr}.join
     end
   end
 
